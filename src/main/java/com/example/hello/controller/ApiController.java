4d46615e0afd30fae369aea02a295382bc825b4e
MO_ON <dev.moongh@gmail.com>
2021-06-28 22:28:43 +0900

    response json

--- src/main/java/com/example/hello/controller/ApiController.java
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -1,8 +1,7 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // 해당 CLASS 는 REST API  처리하는 Controller
 @RequestMapping("/api") // RequestMapping URI 를 지정해주는 Annotation
@@ -11,5 +10,10 @@ public class ApiController {
     @GetMapping("/hello") // http://loaclhost:9090/api/hello
     public String hello() {
         return "hello spring boot!";
+    }
+
+    @PostMapping("/json")
+    public UserDto json(@RequestBody UserDto userDto) {
+        return userDto;
     }
 }
