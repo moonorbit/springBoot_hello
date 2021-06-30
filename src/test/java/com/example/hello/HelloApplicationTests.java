112626be84f86ccd7953647b2d4c10abc4e7b0a4
MO_ON <dev.moongh@gmail.com>
2021-06-30 22:09:48 +0900

    object mapper

--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.hello;
 
+import com.example.hello.dto.UserTestDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloApplicationTests {
-
     @Test
     void contextLoads() {
         System.out.println("hello");
+
+        // Text JSON -> Object
+        // Object -> Text JSON
+
+        // controller req json(text) -> object
+        // response object -> json(text)
+
+        var objectMapper = new ObjectMapper();
+
+        // object -> text
+        // object mapper get method 를 활용한다. (method 명에 get 이 들어가면 error 가 난다)
+        var user = new UserTestDto("steve", 10, "010-1111-2222");
+
+        try {
+            String text = objectMapper.writeValueAsString(user);
+            System.out.println(text);
+
+            // text -> object
+            // object mapper 는 default 생성자를 필요로 한다.
+            var objectUser = objectMapper.readValue(text, UserTestDto.class);
+            System.out.println(objectUser);
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 }
